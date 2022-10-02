14e9d9c0f2d1750a9f7c09ef7554c6a748b6b9d9
Brenovisk <57770889+Brenovisk@users.noreply.github.com>
2022-10-02 16:19:05 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Breno Lucas on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
